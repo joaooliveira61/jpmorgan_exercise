2f48fb3e27e394a5ea375fe28eac708f9e94403d
joaooliveira61 <joao.oliveira.61@gmail.com>
2015-06-16 00:15:50 +0100

    JP Morgan - Commit - Added TradeFactory

--- jpmorgan_test_exercise/src/com/jpmorgan/exerc/test/StockTradeMarketExec.java
+++ jpmorgan_test_exercise/src/com/jpmorgan/exerc/test/StockTradeMarketExec.java
@@ -4,6 +4,8 @@ import java.util.LinkedList;
 import java.util.Random;
 
 import com.jpmorgan.exerc.stock.AbstractStock;
+import com.jpmorgan.exerc.stock.EnumStockType;
+import com.jpmorgan.exerc.stock.StockFactory;
 import com.jpmorgan.exerc.trade.TradeMarket;
 
 public class StockTradeMarketExec {
@@ -11,15 +13,23 @@ public class StockTradeMarketExec {
 	public static void main(String[] args) {
 		TradeMarket stockTradeMarket = new TradeMarket();
 		LinkedList<AbstractStock> listOfStocks = new LinkedList<AbstractStock>();
+		StockFactory stockFactory = new StockFactory();
 		
-		Random lastDividendRandom = new Random();
-		Random fixedDividendRandom = new Random();
-		Random parValueRandom = new Random();
+		String[] stockSymbols = {"TEA", "POP", "ALE", "GIN", "JOE"};
+		EnumStockType[] stockType = {EnumStockType.COMMON, EnumStockType.COMMON,
+				EnumStockType.COMMON, EnumStockType.PREFERRED, EnumStockType.COMMON};
+		int[] lastDividends = {0, 8, 23, 8, 13};
+		int[] fixedDividends = {0, 0, 0, 2, 0};
+		int[] parValues = {100, 100, 60, 100, 250};
 		
-		for (int i = 0; i < 10; i++) {
-			
+		// Create the stocks
+		for (int i = 0; i < 5; i++) {
+			listOfStocks.add(stockFactory.getStock(stockType[i], stockSymbols[i], 
+					lastDividends[i], fixedDividends[i], parValues[i]));
 		}
 
+		// Generate trades
+		
 	}
 
 }
